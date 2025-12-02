731d9032c102ecba1a90f11d015d6de18b609067
Vanshika Hegde <vanshika.hegde@tietoevry.com>
2025-12-02 14:58:22 +0300

    3rd question

---
+++ question3.js
@@ -0,0 +1,13 @@
+const stringValue ="123"
+const convertToNumber = Number(stringValue)
+const finalResult = convertToNumber + 10
+console.log(finalResult);
+
+const booleanValue = true
+const converttoString = String(booleanValue)
+const endResult = converttoString + " is a boolean"
+console.log(endResult);
+
+const value = Number("hello")
+console.log(value);
+
