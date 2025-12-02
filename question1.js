30038e8b097a49efb5a6d2e2d6c314d29ad36dc7
Vanshika Hegde <vanshika.hegde@tietoevry.com>
2025-12-02 14:31:55 +0300

    Initial commit

---
+++ question1.js
@@ -0,0 +1,24 @@
+// Using var
+var var1 = 10
+console.log("Initial var var1:", var1);
+
+// Using let
+let var2 = 20
+console.log("Initial let var2:", var2);
+
+// Using const
+const var3 = 30
+console.log("Initial const var3:", var3);
+
+// Reassigning
+var1 = 100
+console.log("Reassigned var var1:", var1);
+
+var2= 900
+console.log("Reassigned let var2:", var2);
+
+var3 =768
+console.log("Reassigned const var3", var3);
+
+
+// var and let variables can be reassigend but for const its not allowed
