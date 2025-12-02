849e4ecf8aaeed6144cc99c53735c499db8f06e5
Vanshika Hegde <vanshika.hegde@tietoevry.com>
2025-12-02 15:16:50 +0300

    4th question

---
+++ question4.js
@@ -0,0 +1,8 @@
+function sumNormal(number1, number2) {
+    return number1 + number2;
+}
+console.log(sumNormal(1, 109));
+
+//------------------------------Arrow function----------------------------------------------- 
+const sumArrow = (number1, number2) => number1 + number2;
+console.log(sumArrow(1, 109))
