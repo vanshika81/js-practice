1d941c3f5ccfa24b5105c94aa21efec80bb971cf
Vanshika Hegde <vanshika.hegde@tietoevry.com>
2025-12-02 14:50:11 +0300

    2nd question

---
+++ question2.js
@@ -0,0 +1,16 @@
+if (true) {
+    var var1 = "welcome to Javascript";
+    console.log("Inside the block:", var1);
+}
+
+console.log("Outside the block:", var1);
+
+
+// Using let
+if (true) {
+    let var2 = "welcome to javascript";
+    console.log("Inside the block :", var2);
+}
+console.log("Outside the block:", var2);
+
+// we will get error when let variable is accessed outside of the if block
